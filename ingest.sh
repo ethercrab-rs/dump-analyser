576a666835bcdf5344c9195bec54af0a61540ae9
James Waples <james@wapl.es>
2023-10-10 10:26:17 +0100

    Add script to ingest single CSV

---
+++ ingest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+export PGPASSWORD=ethercrab
+
+FILE=$1
+SCENARIO=$(basename "${FILE%.*}")
+CSV="${FILE%.pcapng}.csv"
+
+echo "Processing $FILE (scenario $SCENARIO, save to $CSV)"
+
+# Process pcap into csv
+cargo run --release -- --cycle-packets 6 $FILE
+
+psql -h localhost -U ethercrab  -c "insert into cycles (scenario) values ('${SCENARIO}') on conflict do nothing;"
+
+# Clean out old data for this scenario
+psql \
+    -h localhost \
+    -U ethercrab  \
+    -c "delete from ethercrab where scenario = '${SCENARIO}';"
+
+# Then import it into postgres
+psql \
+    -h localhost \
+    -U ethercrab  \
+    -c "\copy ethercrab(scenario, packet_number, index, command, tx_time_ns, rx_time_ns, delta_time_ns) from '${CSV}' delimiter ',' csv header"
--- process-all.sh
+++ process-all.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-export PGPASSWORD=ethercrab
+export RUST_LOG=''
 
-# Process pcaps into csv
 for f in dumps/*.pcapng; do
-    cargo run --release -- --cycle-packets 6 $f
+    ./ingest.sh $f
 done
-
-psql \
-    -h localhost \
-    -U ethercrab  \
-    -c "truncate ethercrab"
-
-# Then import them into postgres
-for f in dumps/*.csv; do
-    psql \
-        -h localhost \
-        -U ethercrab  \
-        -c "\copy ethercrab(scenario, packet_number, index, command, tx_time_ns, rx_time_ns, delta_time_ns) from '$f' DELIMITER E',' csv header"
-done
-
-psql -h localhost -U ethercrab  -c "insert into cycles (scenario) select distinct scenario from ethercrab on duplicate key ignore;"
