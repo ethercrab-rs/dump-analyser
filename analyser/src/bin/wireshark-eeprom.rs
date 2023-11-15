3c389a8ae48aded19f53051f6b00b1ebfeaf49e4
James Waples <james@wapl.es>
2023-11-15 21:58:15 +0000

    Write dumped data to file

--- analyser/src/bin/wireshark-eeprom.rs
+++ analyser/src/bin/wireshark-eeprom.rs
@@ -6,8 +6,13 @@
 use clap::Parser;
 use dump_analyser::PcapFile;
 use env_logger::Env;
-use ethercrab::{Command, RegisterAddress, Writes};
-use std::{path::PathBuf, time::Duration};
+use ethercrab::{Command, Reads, RegisterAddress, Writes};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
 /// Wireshark EtherCAT EEPROM (partial) dump tool.
 #[derive(Parser, Debug)]
@@ -42,10 +47,28 @@ fn main() -> Result<(), ethercrab::error::Error> {
 
     let mut reader = PcapFile::new(&args.file);
 
-    while let Some(packet) = reader.next() {
-        // dbg!(packet.command);
+    // EEPROM maps for each slave, by address
+    let mut slave_maps = HashMap::new();
 
-        let slave_address = u32::from_le_bytes(packet.command.address()) as u16;
+    /// EEPROM data map for a single device.
+    struct SlaveMap {
+        /// EEPROM data map.
+        data: Vec<u8>,
+
+        /// The address set by the master for reading. Data returned from the slave is written into
+        /// [`data`] starting at this address.
+        addr: u16,
+    }
+
+    while let Some(packet) = reader.next() {
+        // EEPROM reader currently only uses FPRD and FPWR so we'll skip anything else.
+        let slave_address = match packet.command {
+            Command::Read(Reads::Fprd { address, .. })
+            | Command::Write(Writes::Fpwr { address, .. }) => address,
+
+            _ => continue,
+        };
+
         let register = packet.command.register().filter(|r| {
             [
                 u16::from(RegisterAddress::SiiConfig),
@@ -56,40 +79,65 @@ fn main() -> Result<(), ethercrab::error::Error> {
             .contains(r)
         });
 
-        if let Some(register) = register {
-            // Detect an address set. 6 byte packet is SII control header and 2x u16 address.
-            let eeprom_addr = if register == u16::from(RegisterAddress::SiiControl)
-                && packet.data.len() == 6
-                && packet.from_master
-            {
-                Some(u16::from_le_bytes(packet.data[2..4].try_into().unwrap()))
-            } else {
-                None
-            };
+        // Skip packets that aren't EEPROM-related
+        let Some(register) = register else { continue };
 
-            let eeprom_data =
-                if register == u16::from(RegisterAddress::SiiData) && !packet.from_master {
-                    // Useful for matching with Wireshark prettyprinting
-                    // let d = packet
-                    //     .data
-                    //     .chunks(2)
-                    //     .map(|chunk| u16::from_le_bytes(chunk.try_into().unwrap()))
-                    //     .collect::<Vec<_>>();
+        // Allocate 65K for each slave's EEPROM. The actual data could be much smaller, but it's
+        // small enough to just use the max address (u16::MAX) instead of tracking how big the vec
+        // should be.
+        let slave_map = slave_maps.entry(slave_address).or_insert(SlaveMap {
+            data: vec![0u8; u16::MAX.into()],
+            addr: 0,
+        });
 
-                    let d = packet.data;
+        // Detect an address set by the master. 6 byte packet is SII control header and 2x u16
+        // address (second is ignored).
+        if register == u16::from(RegisterAddress::SiiControl)
+            && packet.data.len() == 6
+            && packet.from_master
+        {
+            let addr = u16::from_le_bytes(packet.data[2..4].try_into().unwrap());
 
-                    Some(d)
-                } else {
-                    None
-                };
+            // log::info!("{:#06x} Set EEPROM addr to {:#06x}", slave_address, addr);
 
-            eeprom_addr
-                .map(|addr| println!("{:#06x} Set EEPROM addr to {:#06x}", slave_address, addr));
-
-            eeprom_data.map(|d| println!("{:#06x} EEPROM data {:#x?}", slave_address, d));
+            slave_map.addr = addr;
         }
+        // Response from device
+        else if register == u16::from(RegisterAddress::SiiData) && !packet.from_master {
+            // Useful for matching with Wireshark prettyprinting
+            // let d = packet
+            //     .data
+            //     .chunks(2)
+            //     .map(|chunk| u16::from_le_bytes(chunk.try_into().unwrap()))
+            //     .collect::<Vec<_>>();
 
-        // dbg!(slave_address, register);
+            let d = packet.data;
+
+            // log::info!(
+            //     "{:#06x} EEPROM data at {:#06x} {:#x?}",
+            //     slave_address, slave_map.addr, d
+            // );
+
+            slave_map.data[usize::from(slave_map.addr)..][..d.len()].copy_from_slice(&d);
+        }
+    }
+
+    // Now write out each device's EEPROM to a file
+
+    let dir = PathBuf::from("./eeprom-dumps");
+
+    fs::create_dir_all(&dir).expect("Could not create dumps dir");
+
+    for (addr, eeprom) in slave_maps {
+        let mut filename = dir.clone();
+        filename.push(format!("eeprom-{:#06x}.hex", addr));
+
+        log::info!("Write {}", filename.display());
+
+        let mut f = File::create(&filename).expect("Could not open file for writing");
+
+        f.write_all(&eeprom.data)
+            .expect("Failed to write EEPROM dump data");
     }
 
     Ok(())
