37fabac3575d2ae4abbac58b8c4775a0e090f76c
James Waples <james@wapl.es>
2024-02-20 18:44:40 +0000

    Plot a graph

--- analyser-gtk/src/main.rs
+++ analyser-gtk/src/main.rs
@@ -1,12 +1,32 @@
+use std::path::PathBuf;
+
+use dump_analyser::PcapFile;
 use eframe::egui;
 use egui_plot::{Legend, Line, Plot, PlotPoints};
 
 #[derive(Default)]
-struct MyApp {}
+struct MyApp {
+    plots: Vec<[f64; 2]>,
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("left_panel")
+            .resizable(true)
+            .default_width(150.0)
+            .width_range(80.0..=200.0)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.heading("Left Panel");
+                });
+
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    // lorem_ipsum(ui);
+                });
+            });
+
         let mut plot_rect = None;
+
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Save Plot").clicked() {
                 ctx.send_viewport_cmd(egui::ViewportCommand::Screenshot);
@@ -15,50 +35,47 @@ impl eframe::App for MyApp {
             let my_plot = Plot::new("My Plot").legend(Legend::default());
 
             // let's create a dummy line in the plot
-            let graph: Vec<[f64; 2]> = vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0]];
+            // let graph: Vec<[f64; 2]> = vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0]];
             let inner = my_plot.show(ui, |plot_ui| {
-                plot_ui.line(Line::new(PlotPoints::from(graph)).name("curve"));
+                plot_ui.line(Line::new(PlotPoints::new(self.plots.clone())).name("curve"));
             });
+
             // Remember the position of the plot
             plot_rect = Some(inner.response.rect);
         });
 
-        // Check for returned screenshot:
-        let screenshot = ctx.input(|i| {
-            for event in &i.raw.events {
-                if let egui::Event::Screenshot { image, .. } = event {
-                    return Some(image.clone());
-                }
-            }
-            None
-        });
+        // // Check for returned screenshot:
+        // let screenshot = ctx.input(|i| {
+        //     for event in &i.raw.events {
+        //         if let egui::Event::Screenshot { image, .. } = event {
+        //             return Some(image.clone());
+        //         }
+        //     }
+        //     None
+        // });
     }
 }
 
 fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
-    // let options = eframe::NativeOptions {
-    //     viewport: egui::ViewportBuilder::default().with_inner_size([350.0, 200.0]),
-    //     ..Default::default()
-    // };
-
-    // eframe::run_native(
-    //     "My egui App with a plot",
-    //     options,
-    //     Box::new(|_cc| Box::<MyApp>::default()),
-    // )
+    env_logger::init();
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_inner_size([1920.0, 1080.0])
+            .with_min_inner_size([1280.0, 720.0]),
         ..Default::default()
     };
+
+    let graph = PcapFile::new(&PathBuf::from("./dumps/smol-io-uring-new.pcapng"))
+        .match_tx_rx()
+        .into_iter()
+        .enumerate()
+        .map(|(i, stat)| [i as f64, stat.delta_time.as_nanos() as f64 / 1000.0])
+        .collect::<Vec<_>>();
 
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(MyApp {})),
+        Box::new(|cc| Box::new(MyApp { plots: graph })),
     )
 }
